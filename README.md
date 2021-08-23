e68c4528c6d0c0f412bbc2f626d17f5db46642c7
Mangabo  Kolawole <32314456+koladev32@users.noreply.github.com>
2021-08-23 16:54:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# django-auth-react-tutorial
+
+## Create a virtual environment
+
+```shell
+virtualenv --python=/usr/bin/python3.8 venv
+```
+
+## Install requirements
+
+```shell
+pip install -r requirements.txt
+```
+
+## Migrate and Start the server
+
+```shell
+python manage.py migrate
+python manage.py runserver
+```
---
+++ core/user/viewsets.py
@@ -0,0 +1,26 @@
+from core.user.serializers import UserSerializer
+from core.user.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+ from rest_framework import filters
+
+class UserViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get']
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['updated']
+    ordering = ['-updated']
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all()
+
+    def get_object(self):
+        lookup_field_value = self.kwargs[self.lookup_field]
+
+        obj = User.objects.get(lookup_field_value)
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
