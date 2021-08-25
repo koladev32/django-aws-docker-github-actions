b5b45d4bb2c9b34d317c79563bf7c3c9efb594fd
koladev <onaelmangabo@gmail.com>
2021-08-25 15:42:49 +0100

    - feat: add django environ package

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+# Initialise environment variables
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
