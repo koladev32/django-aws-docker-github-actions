b059a4590da787351bc59301b91942ae7965fbdc
koladev <onaelmangabo@gmail.com>
2021-08-25 16:26:36 +0100

    - fix: remove public_id, created, updated

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -3,10 +3,8 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['public_id', 'username', 'email', 'is_active', 'created', 'updated']
+        fields = ['username', 'email', 'is_active']
         read_only_field = ['is_active']
